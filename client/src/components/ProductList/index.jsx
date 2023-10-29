8d71ebd7f267e73ca3e99a5b5b23f6bb3289e7e7
lightoftwelve <info@lightoftwelve.com>
2023-10-29 01:03:38 -0400

    updated productlist to use redux

--- client/src/components/ProductList/index.jsx
+++ client/src/components/ProductList/index.jsx
@@ -1,44 +1,51 @@
-import { useEffect } from 'react';
-import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
-import { useQuery } from '@apollo/client';
-import { QUERY_PRODUCTS } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
-import spinner from '../../assets/spinner.gif';
+import { useEffect } from "react";
+
+// Redux Imports
+import { useSelector, useDispatch } from "react-redux";
+import { updateProducts } from "../../redux/slices/productsSlice";
+
+// Apollo Imports
+import { useQuery } from "@apollo/client";
+import { QUERY_PRODUCTS } from "../../utils/queries";
+import { idbPromise } from "../../utils/helpers";
+
+// Component & Asset Imports
+import ProductItem from "../ProductItem";
+import spinner from "../../assets/spinner.gif";
 
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch(); // Setting up the dispatch function to send actions to the Redux store
 
-  const { currentCategory } = state;
+  // Extracting the current category and products from the Redux store
+  const { currentCategory } = useSelector((state) => state.category);
+  const { products } = useSelector((state) => state.products);
 
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
+  // Fetching product data based on the current category using Apollo Client
+  const { loading, data } = useQuery(QUERY_PRODUCTS, {
+    variables: { category: currentCategory },
+  });
 
   useEffect(() => {
+    // If data is available, update the products in the Redux store and save to IndexedDB
     if (data) {
-      dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products,
-      });
+      dispatch(updateProducts(data.products));
       data.products.forEach((product) => {
-        idbPromise('products', 'put', product);
+        idbPromise("products", "put", product);
       });
+      // If loading is false and no data is available, retrieve products from IndexedDB
     } else if (!loading) {
-      idbPromise('products', 'get').then((products) => {
-        dispatch({
-          type: UPDATE_PRODUCTS,
-          products: products,
-        });
+      idbPromise("products", "get").then((products) => {
+        dispatch(updateProducts(products));
       });
     }
   }, [data, loading, dispatch]);
 
+  // Filter products based on the current category
   function filterProducts() {
     if (!currentCategory) {
-      return state.products;
+      return products;
     }
-
-    return state.products.filter(
+    return products.filter(
       (product) => product.category._id === currentCategory
     );
   }
@@ -46,16 +53,12 @@ function ProductList() {
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
-      {state.products.length ? (
+      {products.length ? (
         <div className="flex-row">
           {filterProducts().map((product) => (
             <ProductItem
               key={product._id}
-              _id={product._id}
-              image={product.image}
-              name={product.name}
-              price={product.price}
-              quantity={product.quantity}
+              {...product} // Simplified prop passing using the spread operator
             />
           ))}
         </div>
