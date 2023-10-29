5ae8a1a269be9f8208e6c6c6b1a1bf86941a8f47
lightoftwelve <info@lightoftwelve.com>
2023-10-29 01:15:37 -0400

    updated success.jsx to use redux

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -1,35 +1,47 @@
-import { useEffect } from 'react';
-import { useMutation } from '@apollo/client';
-import Jumbotron from '../components/Jumbotron';
-import { ADD_ORDER } from '../utils/mutations';
-import { idbPromise } from '../utils/helpers';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useMutation } from "@apollo/client";
+import Jumbotron from "../components/Jumbotron";
+import { ADD_ORDER } from "../utils/mutations";
+import { idbPromise } from "../utils/helpers";
+import { clearCart } from "../redux/slices/cartSlice";
 
 function Success() {
-  const [addOrder] = useMutation(ADD_ORDER);
+  const dispatch = useDispatch();
+  const [addOrder] = useMutation(ADD_ORDER); // Using Apollo Client’s useMutation hook to send a mutation to the server.
 
+  // useEffect hook runs when the component mounts to the DOM.
   useEffect(() => {
+    // Define an async function to save the order.
     async function saveOrder() {
-      const cart = await idbPromise('cart', 'get');
+      const cart = await idbPromise("cart", "get");
       const products = cart.map((item) => item._id);
 
+      // Check if there are products to be ordered.
       if (products.length) {
+        // Send the ADD_ORDER mutation to the server with the product IDs.
         const { data } = await addOrder({ variables: { products } });
         const productData = data.addOrder.products;
 
+        // Iterate over the ordered products and delete them from the cart in IndexedDB.
         productData.forEach((item) => {
-          idbPromise('cart', 'delete', item);
+          idbPromise("cart", "delete", item);
         });
+
+        dispatch(clearCart()); // Dispatch the clearCart action to update the global state
       }
 
+      // Set a timeout to redirect the user to the home page after 3 seconds.
       setTimeout(() => {
-        window.location.assign('/');
+        window.location.assign("/");
       }, 3000);
     }
 
     saveOrder();
-  }, [addOrder]);
+  }, [addOrder, dispatch]);
 
   return (
+    // Render a success message using the Jumbotron component.
     <div>
       <Jumbotron>
         <h1>Success!</h1>
