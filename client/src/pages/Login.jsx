e920003eeca28ba3c4bbcb82c4d24cc5e96daf81
lightoftwelve <info@lightoftwelve.com>
2023-10-29 01:14:00 -0400

    updated login page to use redux

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,26 +1,42 @@
-import { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom';
-import { LOGIN } from '../utils/mutations';
-import Auth from '../utils/auth';
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import { LOGIN } from "../utils/mutations"; // Importing GraphQL mutation for logging in
+import Auth from "../utils/auth"; // Utility for handling authentication tokens
+import { setUser, setError } from "../redux/slices/userSlice"; // Importing actions from userSlice for dispatching
 
 function Login(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
-  const [login, { error }] = useMutation(LOGIN);
+  const [formState, setFormState] = useState({ email: "", password: "" }); // Local state to hold form input data
+  const [login] = useMutation(LOGIN); // Apollo Client hook for executing the LOGIN mutation
+  const dispatch = useDispatch(); // Getting the dispatch function from Redux hook to dispatch actions
+  const error = useSelector((state) => state.user.error); // Selecting the error state from the Redux store
 
+  // Function to handle form submission
   const handleFormSubmit = async (event) => {
-    event.preventDefault();
+    event.preventDefault(); // Preventing the default form submission behavior
     try {
+      // Executing the login mutation with the form data
       const mutationResponse = await login({
         variables: { email: formState.email, password: formState.password },
       });
-      const token = mutationResponse.data.login.token;
-      Auth.login(token);
+
+      // If login is successful, store token and update user state in Redux store
+      if (mutationResponse.data.login) {
+        const token = mutationResponse.data.login.token;
+        Auth.login(token);
+        dispatch(setUser({ email: formState.email }));
+      } else {
+        throw new Error("Login failed");
+      }
     } catch (e) {
-      console.log(e);
+      // If login fails, set error state in Redux store
+      dispatch(setError("The provided credentials are incorrect"));
     }
   };
 
+  // Function to handle form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormState({
@@ -57,7 +73,7 @@ function Login(props) {
         </div>
         {error ? (
           <div>
-            <p className="error-text">The provided credentials are incorrect</p>
+            <p className="error-text">{error}</p>
           </div>
         ) : null}
         <div className="flex-row flex-end">
